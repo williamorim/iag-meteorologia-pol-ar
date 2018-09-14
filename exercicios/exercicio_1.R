51ee5b87a5bfbf16d0b04ccb24711e2ec75eedc6
williamorim <thewilliam89@gmail.com>
2018-09-14 10:48:20 -0300

    implementando exemplo 1

--- exercicios/exercicio_1.R
+++ exercicios/exercicio_1.R
@@ -1,3 +1,12 @@
+# Carregando pacotes
+library(tidyverse)
+
+# caso o pacote tidyverse não esteja instalado, rodar o código abaixo
+# pode demorar um pouco, principalmente no Linux
+install.packages(tidyverse) 
+
+# Valores no passo 0
+
 o3 <- 0.15
 no2 <- 0.20
 no3 <- 0
@@ -5,14 +14,25 @@ n2o5 <- 0
 hno3 <- 0
 h2o <- 22.84
 
-passo <- 600
+# Tamanho do passo
 
-k1 <- 0.0468 / passo
-k2 <- 2510 / passo
-k3 <- 3.14e-6 / passo
-k4 <- 1.92e-6 / passo
+passo <- 1/600 # minuto -> décimo de segundo
+
+# Constantes de reação transformadas para o tamanho do passo
+
+k1 <- 0.0468 * passo
+k2 <- 2510 * passo
+k3 <- 3.14e-6 * passo
+k4 <- 1.92e-6 * passo
+
+# 1 milhão de passos
+
+n_passos <- 1000000
+
+# Iterações 
+# (começa do dois porque vetores no R começam com o índice 1, que já tem o passo 0)
   
-for(i in 2:1000000) {
+for(i in 2:n_passos) {
   
   o3[i] <- o3[i - 1] - k1 * o3[i-1] * no2[i-1]
   
@@ -34,27 +54,24 @@ for(i in 2:1000000) {
   
 }
 
-library(tidyverse)
-
+# Transformando em data.frame
 df <- tibble(o3, no2, no3, n2o5, hno3, h2o)
 
+# Fazendo gráficos
 
-
+# Todas as curvas no mesmo gráfico (tirando a água por causa da escala)
 df %>% 
-  tidyr::gather(composto, valor, -h2o) %>%
+  gather(composto, valor, -h2o) %>%
   group_by(composto) %>% 
-  dplyr::mutate(passo = 1:n()) %>%
-  #filter(passo < 10000) %>% 
+  mutate(passo = 1:n()) %>%
 ggplot() +
   geom_line(aes(x = passo, y = valor, color = composto))
 
+# Cada curva em um gráfico
 df %>% 
-  tidyr::gather(composto, valor, everything()) %>%
+  gather(composto, valor, everything()) %>%
   group_by(composto) %>% 
-  dplyr::mutate(passo = 1:n()) %>%
+  mutate(passo = 1:n()) %>%
   ggplot() +
   geom_line(aes(x = passo, y = valor)) +
   facet_wrap(~composto, scales = "free_y")
-
-ggplot(df) +
-  geom_line(aes(x = 1:1000000, y = no3))
